3c663f32cb3c6c9d28916302a889adb9d2b406e0
Vaibhavtiwari079 <vaibhavtiwari.0504@gmail.com>
2024-05-22 19:43:26 +0530

    new

--- utils/requests.js
+++ utils/requests.js
@@ -1,19 +1,24 @@
 const apiDomain =process.env.NEXT_PUBLIC_API_DOMAIN ||null;
 
 async function fetchProperties({showFeatured=false}={}){
-    try {
-      
-      if(!apiDomain){
-        return [];
-      }
-      const res= await fetch(`${apiDomain}/properties${showFeatured?'/featured':''}`,{
-        cache:'no-store'
-      })
-      if(!res.ok){
-        throw new Error('failed to fetch data');
-      }
-      return res.json();
-    } catch (error) {
+  try {
+    // Handle the case where the domain is not available yet
+    if (!apiDomain) {
+      return [];
+    }
+
+    const res = await fetch(
+      `${apiDomain}/properties${showFeatured ? '/featured' : ''}`,
+      { cache: 'no-store' }
+    );
+
+    if (!res.ok) {
+      console,log(error)
+      throw new Error('Failed to fetch data');
+    }
+
+    return res.json();
+  } catch (error) {
       
       console.log(error)
       return [];
